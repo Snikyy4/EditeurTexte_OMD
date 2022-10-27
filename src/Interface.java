7b775f403bdab2a0bb5f58cbd9a395eef29b8f24
Snikyy4 <lequertierthibaud@gmail.com>
2022-10-27 16:53:29 +0200

    endddd v1

--- src/Interface.java
+++ src/Interface.java
@@ -3,8 +3,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 import java.awt.Dimension;
 import java.awt.event.*;
